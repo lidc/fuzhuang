8111bde65a94ef8c15778ee929ac39998a5f8eb2
lidc <876890079@qq.com>
2014-12-26 18:45:22 +0800

    company update

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,11 +1,5 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    'TMPL_PARSE_STRING' => array(
-        '__PUBLICA__' => __ROOT__.'/Application/Admin/Public'
-    ),    
-    'TMPL_L_DELIM' => '{{',
-    'TMPL_R_DELIM' => '}}',
-    '__FILE__' => __ROOT__.'/index.php/',
-    'SESSION_AUTO_START' =>true,
+    
 );
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,19 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'TMPL_PARSE_STRING' => array(
+        '__PUBLICA__' => __ROOT__.'/Application/Admin/Public'
+    ),    
+    'TMPL_L_DELIM' => '{{',
+    'TMPL_R_DELIM' => '}}',
+    '__FILE__' => __ROOT__.'/index.php/',
+    'SESSION_AUTO_START' =>true,
+	
+	'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'fuzhuang',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  'root',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  '',    // 数据库表前缀
 );

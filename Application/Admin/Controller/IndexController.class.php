1fa37f987f9804ff0cd3cab3712c8ccc49ccc39a
lidc <876890079@qq.com>
2015-01-01 00:11:12 +0800

    20150101

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,22 +3,34 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $data = M('nav_page');
-        $list = $data->select();
-        $this->assign("vo",$list);
-        $this->display('login');
+        $username = isset($_SESSION['fuzhuang']['username']) ? $_SESSION['fuzhuang']['username'] : "";
+        if(!empty($username)){
+            $this->display('index');
+        }else{
+            $this->display('login');    
+        }        
     }
     public function login(){
-    	$username = isset($_POST['username']) ? in($_POST['username']) : "";
-    	$validatecode = isset($_POST['validatecode']) ? $_POST['validatecode'] : 0;  
-    	$public  = new PublicController();  
+        if($_POST){
+        	$username = isset($_POST['username']) ? in($_POST['username']) : "";
+            $password = isset($_POST['userpwd']) ? md5($_POST['userpwd']) : "";
+        	$validatecode = isset($_POST['validatecode']) ? $_POST['validatecode'] : 0;  
+        	$public  = new PublicController();  
+        	if(!$public->check($validatecode)){
+                $this->show("<script>alert('验证码错误，请重新输入！');javascript:history.go(-1);</script>");    
+                exit;      
+            }
+            $this->assign("__FILE__",C('__FILE__'));
+            $data = M('user_admin');
+            $list = $data->field('password')->where("username='".$username."'")->find();
 
-    	if(!$public->check($validatecode)){
-            $this->show("<script>alert('验证码错误，请重新输入！');javascript:history.go(-1);</script>");
-            exit();           
-        }
-        
-    	$this->assign("__FILE__",C('__FILE__'));
-    	$this->display('index');
+            if($list['password']!=$password){
+                $this->show("<script>alert('用户名或密码有误，请重新输入！');javascript:history.go(-1);</script>");    
+                exit;  
+            }else{
+                $_SESSION['fuzhuang']['username'] = $username;
+                $this->display('index');
+            }
+        }   	
     }
 }

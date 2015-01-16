bb7a8ca64f5d6d29a4543cf3aca7641db4443425
lidc <876890079@qq.com>
2015-01-16 18:22:12 +0800

    菜单修改

--- Application/Runtime/Cache/Admin/e0e54c48c40814ba365b40ae3dec3a06.php
+++ Application/Runtime/Cache/Admin/e0e54c48c40814ba365b40ae3dec3a06.php
@@ -33,43 +33,74 @@
     </style>
 </head>
 <body>
-<form action="index.html" method="post" class="definewidth m20">
-<input type="hidden" name="id" value="{$menu.id}" />
+<form action="edit" method="post" class="definewidth m20" enctype="multipart/form-data">
+<input type="hidden" name="id" value="<?php echo ($nl["id"]); ?>" />
 <table class="table table-bordered table-hover m10">
     <tr>
         <td width="10%" class="tableleft">上级</td>
         <td>
-            <select name="parentid">
-            <option value="0">一级菜单</option><option value='1'   />&nbsp;系统管理</option><option value='7'   />&nbsp;明信片管理</option>            </select>
+            <select name="parentid"><?php echo ($nl['pid']); ?>
+                <option value="0">&nbsp;一级菜单</option>
+                <?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$v): $mod = ($i % 2 );++$i;?><option value='<?php echo ($v['id']); ?>' <?php if($v['id'] == $nl['pid']): ?>selected<?php endif; ?> />&nbsp;<?php echo ($v['nav_title']); ?></option><?php endforeach; endif; else: echo "" ;endif; ?>                           
+            </select>
         </td>
     </tr>
     <tr>
-        <td class="tableleft">名称</td>
-        <td><input type="text" name="name"/></td>
+        <td class="tableleft">菜单名称</td>
+        <td><input type="text" name="nav_title" value="<?php echo ($nl['nav_title']); ?>" /></td>
     </tr>
     <tr>
-        <td class="tableleft">Group</td>
-        <td><input type="text" name="group"/></td>
+        <td class="tableleft">菜单简介</td>
+        <td><textarea cols="40" rows="5" name="nav_desc" id="nav_desc" style="width:380px;"><?php echo ($nl['nav_desc']); ?></textarea></td>
     </tr>
     <tr>
-        <td class="tableleft">Model</td>
-        <td><input type="text" name="module"/></td>
+        <td class="tableleft">图片</td>
+        <td>
+            <input type="file" name="nav_photo" id="nav_photo"/>
+            <br/>
+            <?php if($nl['nav_photo'] != ''): ?><img src="<?php echo ($nl['nav_photo']); ?>" style="height:80px;"/><?php endif; ?>
+        </td>
     </tr>
     <tr>
-        <td class="tableleft">Action</td>
-        <td><input type="text" name="action"/></td>
-    </tr>
-    <tr>
-        <td class="tableleft">备注</td>
-        <td><input type="text" name="remark"/></td>
-    </tr>
+        <td class="tableleft">排序</td>
+        <td>
+            <input class="text" type="text" name="nav_order" value="<?php echo ($nl['nav_order']); ?>" style="width:90px;" onblur="if(/[^0123456789]/g.test(value))value=value.replace(/[^0123456789]/g,'');" onkeyup="if(/[^0123456789]/g.test(value))value=value.replace(/[^0123456789]/g,'');"/>
+        </td>
+    </tr>   
     <tr>
         <td class="tableleft">状态</td>
         <td>
-            <input type="radio" name="status" value="0" checked/> 启用
-            <input type="radio" name="status" value="1"/> 禁用
+            <input type="radio" name="nav_status" value="1" <?php if($nl['nav_status'] == 1): ?>checked<?php endif; ?> /> 启用
+            <input type="radio" name="nav_status" value="0" <?php if($nl['nav_status'] == 0): ?>checked<?php endif; ?> /> 禁用
         </td>
     </tr>
+    <tr>
+        <td class="tableleft">页面内容</td>
+        <td>
+            <textarea cols="40" rows="5" name="page_content" id="page_content"><?php echo ($ncl['content']); ?></textarea>
+        </td>
+    </tr>
+    <tr>
+        <td class="tableleft" colspan="2">SEO优化</td>       
+    </tr>
+    <tr>
+        <td class="tableleft">Meta 标题</td>
+        <td>
+            <input type="text" name="meta_title" style="width:380px;" value="<?php echo ($nl['meta_title']); ?>" />
+        </td>
+    </tr>
+    <tr>
+        <td class="tableleft">Meta 关键字</td>
+        <td>
+            <input type="text" name="meta_keywords" style="width:380px;" value="<?php echo ($nl['meta_keywords']); ?>" />
+        </td>
+    </tr>
+    <tr>
+        <td class="tableleft">Meta 描述</td>
+        <td>
+            <input type="text" name="meta_description" style="width:380px;" value="<?php echo ($nl['meta_description']); ?>" />
+        </td>
+    </tr>    
     <tr>
         <td class="tableleft"></td>
         <td>
@@ -88,3 +119,4 @@
 
     });
 </script>
+<?php echo ($editor); ?>
